12b815901531d54efb038669e040412a53f0e780
cilun@qq.com <cilun@qq.com>
2022-06-28 00:40:49 +0800

    修正目录名repair dir name

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,10 +4,10 @@
  * @since 2022-03-15
  */
 import http from '@/service/http'
-import * as SAuth from './moudules/auth/auth'
-import * as SAdmin from './moudules/admin/admin'
-import * as SLogAdminOpr from './moudules/log/adminopr'
-import * as SBanner from './moudules/banner/index'
+import * as SAuth from './modules/auth/auth'
+import * as SAdmin from './modules/admin/admin'
+import * as SLogAdminOpr from './modules/log/adminopr'
+import * as SBanner from './modules/banner/index'
 
 export {
   http,
